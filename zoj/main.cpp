76ebe08cb12546271356d1937e798752fa807fb4
xsun89 <sunxin89@gmail.com>
2015-06-11 09:44:03 -0700

    add

--- zoj/main.cpp
+++ zoj/main.cpp
@@ -5,17 +5,41 @@
 #include <stdio.h>
 
 using namespace std;
-
 bool iszoj(const char *source) {
     bool isZ = false;
     bool isO = false;
     bool isJ = false;
     int len = strlen(source);
     int i = 0;
+    while(i<len){
+        if(i==0 && source[i] == 'z'){
+            isZ = true;
+        }
+        if(i==1 && source[i] == 'o'){
+            isO = true;
+        }
+        if(i==2 && source[i] == 'j'){
+            isJ = true;
+        }
+
+        i++;
+    }
+    if (isZ && isO && isJ && i==3) {
+        return true;
+    }
+
+    return false;
+}
+bool isxzojx(const char *source) {
+    int len = strlen(source);
+    int i = 0;
     int numberZ = 0;
     int positionZ = -1;
     int numberJ = 0;
+    int numberO = 0;
     int positionJ = -1;
+    int positionO = -1;
+    bool ret = true;
     while (i < len) {
         if (source[i] == 'z') {
             numberZ++;
@@ -25,23 +49,39 @@ bool iszoj(const char *source) {
             numberJ++;
             positionJ = i;
         }
-        if (source[i] == 'j' && i == 2) {
-            isJ = true;
+        /*
+        if (i > positionZ && source[i] == 'o') {
+            numberO++;
+            positionO = i;
         }
+        */
         i++;
     }
-
-    if (numberJ == 1 && numberZ == 1 && positionJ - positionZ >= 1) {
-        return true;
+    int k = 0;
+    int h = len-1;
+    for(;k<positionZ && h>positionJ;k++,h--){
+        if(source[k] != source[h]){
+            ret = false;
+        }
+        if(source[k] != 'o' && source[k] != ' '){
+            ret = false;
+        }
     }
 
-    return false;
+    if (numberJ != 1 || numberZ != 1 || positionJ - positionZ != 2 || source[positionZ+1] != 'o') {
+        ret = false;
+    }
+
+    return ret;
 }
 
 int main() {
     char source[100];
     cin >> source;
-    bool ret = iszoj(source);
+    bool ret;
+    //bool ret = iszoj(source);
+    //cout << ret << endl;
+    ret = isxzojx(source);
     cout << ret << endl;
     return 0;
 }
